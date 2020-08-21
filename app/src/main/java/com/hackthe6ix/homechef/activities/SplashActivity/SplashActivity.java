f2c4983b29e4a4d877fa387ec38858f1277a79c6
Helen Zhang <759787005hyz@gmail.com>
2020-08-21 00:00:42 -0400

    Added AccountActivity, LocationActivity, RecipeActivity and SplashActivity

---
+++ app/src/main/java/com/hackthe6ix/homechef/activities/SplashActivity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.hackthe6ix.homechef.activities.SplashActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackthe6ix.homechef.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
