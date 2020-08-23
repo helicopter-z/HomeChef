2339f095b663eff4b84e3e620151b38ad05de185
Helen Zhang <759787005hyz@gmail.com>
2020-08-22 22:54:41 -0400

    Revert "Fixed Splash behaviour"
    This reverts commit 3f29bd1736bfcc8000fbe72a8bb81b8a7fa75e14.

--- app/src/main/java/com/hackthe6ix/homechef/activities/SplashActivity.java
+++ app/src/main/java/com/hackthe6ix/homechef/activities/SplashActivity.java
@@ -11,7 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash)
         if(AccountActivity.getUserName(SplashActivity.this).length() == 0)
         {
             // call Login Activity - user not yet logged in
